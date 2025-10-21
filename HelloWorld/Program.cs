f0bf7447937723501bd4bb290e55918db1d69c97
nwietzo <shadic3b2012@gmail.com>
2025-10-21 23:04:38 +0300

    update code HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            if (text == "Hallo" || text == "Hi")
+            {
+                Console.WriteLine("Wow, Hi, User!");
+            }
+            else if (text == "Привет")
+            {
+                Console.WriteLine("Привет Мир!");
+            }
+            else
+            {
+                Console.WriteLine("Hallo World!");
+            }
+        }
+    }
+}
